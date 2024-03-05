c9bb2b20cc0b2194a4e8661b8c4780046690e9a6
metal <metaldeth.alex@gmail.com>
2024-03-05 13:35:25 +0500

    Reviewed the code and wrote suggestions

--- src/components/ChoiseGroup/types.ts
+++ src/components/ChoiseGroup/types.ts
@@ -1,14 +1,34 @@
 import { Size } from "../../types/size";
 import { ViewFull } from "../../types/view";
 
+/* TODO: Нужно переименовать Choise => Choice */
+
 /**
- * Форма (Варианты)
+ * Форма компонента ChoiseGroup
  */
 export type FormChoiseGroup = 'round' | 'brick' | 'default';
 
+// TODO: посмотри в сторону enum, возможно тебе понравится
+export enum FormChoiseGroupEnum {
+  round = 'round',
+  brick = 'brick',
+  default = 'default'
+}
+
+// TODO: не хватает комментария для списка
+/**
+ * Список форм компонента ChoiseGroup
+ */
 export const FormChoiseGroupList: FormChoiseGroup[] = ['round', 'brick', 'default'];
 
+// TODO: не хватает комментария для типа
+/**
+ * Свойства компонента ChoiseGroup
+ */
 export type ChoiseGroupProps = {
+  /**
+   * Список кнопок
+   */
   list: string[];
   /**
    * Выбранный элемент
@@ -22,6 +42,7 @@ export type ChoiseGroupProps = {
    * Вид элемента
    */
   view?: ViewFull;
+  // TODO: по аналогии с верхними элементами лучше написать комментарий "Форма элемента"
   /**
    *  Форма
    */
